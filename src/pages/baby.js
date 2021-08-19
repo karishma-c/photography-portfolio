7ebc38401d8c7f8308a31e8cf60eb1971daf87cb
karishma-c <karishmadgct@gmail.com>
2021-08-19 13:32:22 +0530

    added carousel functionality in modal

--- src/pages/baby.js
+++ src/pages/baby.js
@@ -8,6 +8,8 @@ import Three from './../assets/images/babies/3.jpg'
 import Four from './../assets/images/babies/4.jpg'
 import Five from './../assets/images/babies/5.jpg'
 import Six from './../assets/images/babies/6.jpg'
+import leftArrow from './../assets/images/left-arrow.svg'
+import rightArrow from './../assets/images/right-arrow.svg'
 import {
     babyImageSection,
     heading,
@@ -15,9 +17,12 @@ import {
     imageDiv,
     modal,
     modalContent,
+    modalContainer,
     modalImage,
     closeIcon,
-    image
+    image, 
+    prev,
+    next
 } from './../styles/babies.module.css'
 import './../styles/babies.module.css'
 
@@ -57,9 +62,12 @@ const BabyPage = () => {
             <div id="myModal" className={modal}>
                 <div id="modal-content" className={modalContent} >
                     <span id="close" className={closeIcon} onClick={hideModal}>&times;</span>
-                    <div className={modalImage}>
-                        <img id="image" className={image} src="" alt="image"></img>
-                        
+                    <div className={modalContainer}>
+                        <img id="left-arrow" className={prev} src={leftArrow} alt="image"></img>
+                        <div className={modalImage}>
+                           <img id="image" className={image} src="" alt="image"></img>
+                        </div>
+                        <img id="right-arrow" className={next} src={rightArrow} alt="image"></img>
                     </div>
                 </div>
             </div>
@@ -72,13 +80,43 @@ const BabyPage = () => {
         Image.src = e.target.src;
         let modal = document.getElementById("myModal");
         modal.style.display = "block";
+        let imageDiv = document.querySelectorAll("#Imagediv");
+        let leftArrow = document.getElementById("left-arrow");
+        let rightArrow = document.getElementById("right-arrow");
+        rightArrow.addEventListener("click", () => {
+            next();
+        })
+        leftArrow.addEventListener("click", () => {
+            previous();
+        })
+        var num=0;
+        function next()
+        {
+            num++;
+            //checking whether the new position of image is greater or equal to the no. of images we use
+            if(num >= imageDiv.length)//images is the defined arary
+            {
+               num=0;
+               //if the condition is true num is defined to hold to first image of the slider
+            }
+            Image.src=imageDiv[num].src;
+        }
+        function previous()
+        {
+            num--;
+            //checking whether the new position of image is bfore or after the first image 
+            if(num < 0)//images is the defined arary
+            {
+                num=imageDiv.length-1; //if the condition is true num is defined to hold to last image of the slider
+            }
+            Image.src=imageDiv[num].src;
+        }
     }
     function hideModal() {
         let modal = document.getElementById("myModal");
         modal.style.display = "none";
     }
 }
-  
-  
+   
 export default BabyPage
 
